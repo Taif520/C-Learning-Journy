1770292fa4d93b1c4d3e6e5f86980d7b4ec2f6bb
Taif331 <oman56yy@gmail.com>
2026-04-28 19:53:41 +0400

    Lesson1 Done

---
+++ Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+                   /*Learn variables and data types*/
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to C# world");
+            /*when I want to ask the user to type something*/
+            Console.ReadLine("Enter your name: ");
+
+            Console.WriteLine("data types");
+            /*Integir*/
+            int a = 35;
+            Console.WriteLine(a);
+            double b = 23.5;
+            Console.WriteLine(b);
+            /*We use F to show that is float number otherwise the compiler will think that is double number*/
+            float c = 23.5F;
+            Console.WriteLine(b);
+
+        }
+    }
+}
